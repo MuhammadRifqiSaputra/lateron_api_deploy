2127913666b7315f6043c6d131ee5fa693804bd7
MuhammadRifqiSaputra <muhammadrifqi.23002@mhs.unesa.ac.id>
2026-06-05 23:15:41 +0700

    Fix AuthPanel import

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthPanel from "../components/AuthPanel";
+import AuthPanel from "../components/Authpanel";
 
 const EyeOffIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
